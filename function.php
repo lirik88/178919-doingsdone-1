007a054c789b0a0750e87b06ce27bec29625d1a9
Kirill Batorin <lirik88@list.ru>
2017-11-17 17:38:12 +0300

    project template 2

---
+++ function.php
@@ -0,0 +1,29 @@
+<?php
+//Функция подключения шаблона
+function renderTemplate (string $viewPath, string $layoutPath, array $list){
+	$html = '';
+	if (!empty($viewPath) && file_exists($viewPath) && !empty($layoutPath) && file_exists($layoutPath)){
+		ob_start();
+		extract($list);
+		include($viewPath);
+		$content = ob_get_contents();
+		ob_clean();
+		include($layoutPath);
+		$html = ob_get_contents();
+		ob_end_clean();
+	}
+	return $html;
+}
+//Функция подчсчета задач в проекте
+function getNumberOfTasks(array $tasks, string $project) {
+	$result = 0;
+	if ($project === 'Все') {
+		return count($tasks);
+	}
+	foreach ($tasks as $task) {
+		if ($task['project'] === $project) {
+			$result++;
+		}
+	}
+	return $result;
+}
