ecbf0b2eb7c79f7d2b95d375cb58f6c977fd0537
Kirill Batorin <lirik88@list.ru>
2017-12-02 13:27:44 +0300

    schema last edit))

--- schema.sql
+++ schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE users (
   created_at    TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   email         CHAR(128) NOT NULL UNIQUE,
   name          CHAR(128) NOT NULL,
-  password      CHAR(60) NOT NULL,
+  password      CHAR(32) NOT NULL,
   userpic       TEXT DEFAULT NULL,
   is_deleted    TINYINT(1) DEFAULT 0,
 
@@ -18,7 +18,6 @@ CREATE TABLE tasks (
   id              INT AUTO_INCREMENT PRIMARY KEY,
   project_id      INT,
   name            CHAR(128) NOT NULL UNIQUE,
-  user_id         INT,
   created_at      TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   completed_at    TIMESTAMP DEFAULT NULL,
   is_completed    TINYINT(1) DEFAULT 0,
@@ -27,7 +26,6 @@ CREATE TABLE tasks (
 
   INDEX taskName (name),
   FOREIGN KEY (project_id) REFERENCES projects(id),
-  FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE projects (
