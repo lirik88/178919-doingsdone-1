334376d3fbca899c9366c0bb4f3ab92ff5cb9616
Kirill Batorin <lirik88@list.ru>
2017-12-02 21:05:28 +0300

    queries3

--- queries.sql
+++ queries.sql
@@ -12,9 +12,9 @@ VALUES
 INSERT INTO
   users (email, name, password, userpic)
 VALUES
-  ('ignat.v@gmail.com', 'Игнат', '$2y$10$OqvsKHQwr0Wk6FMZDoHo1uHoXd4UdxJG/5UDtUiie00XaxMHrW8ka', '/img/user-pic.jpg'),
-  ('kitty_93@li.ru', 'Леночка', '$2y$10$bWtSjUhwgggtxrnJ7rxmIe63ABubHQs0AS0hgnOo41IEdMHkYoSVa', NULL),
-  ('warrior07@mail.ru', 'Руслан', '$2y$10$2OxpEH7narYpkOT1H5cApezuzh10tZEEQ2axgFOaKW.55LxIJBgWW', NULL);
+  ('ignat.v@gmail.com', 'Игнат', MD5('ug0GdVMi'), '/img/user-pic.jpg'),
+  ('kitty_93@li.ru', 'Леночка', MD5('daecNazD'), NULL),
+  ('warrior07@mail.ru', 'Руслан', MD5('oixb3aL8'), NULL);
 
 # Список задач
 INSERT INTO
